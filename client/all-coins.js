c6eb8568085844e7d1a636e1f3f416879ae9d609
Gabriel Lebec <glebec@gmail.com>
2018-08-31 10:36:28 -0400

    day 3: react forms for coin names

--- client/all-coins.js
+++ client/all-coins.js
@@ -5,7 +5,17 @@ import { requestCoins } from './store'
 export class AllCoins extends React.Component {
     componentDidMount() {
         this.props.getInitialCoins()
+        this.mkHandleSubmit.bind(this)
     }
+
+    mkHandleSubmit (id) {
+        return (event) => { // <-- THIS FUNCTION is the `handleSubmit`
+            event.preventDefault()
+            const newName = event.target.nom.value
+            console.log('Submitted new coin form! id & name:', id, newName)
+        }
+    }
+
     render() {
         if (this.props.loading) {
             return <p>Loading, please wait…</p>
@@ -24,7 +34,17 @@ export class AllCoins extends React.Component {
                 if (!origin) return true
                 return coin.origin.name === origin // could make this more lenient e.g. with `toLowerCase`
             })
-            .map(coin => <li key={coin.id}>{coin.name}</li>)
+            .map(coin => {
+                return (
+                    <li key={coin.id}><div>
+                        <p>{coin.name}</p>
+                        <form onSubmit={ this.mkHandleSubmit(coin.id) }>
+                            <input name="nom" />
+                            <button type="submit">Change Name</button>
+                        </form>
+                    </div></li>
+                )
+            })
         return <ul>{allCoins}</ul>
     }
 }
