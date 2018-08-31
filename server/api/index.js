39b548b65e98b268dd7ba1550cecfa6f992a626c
Gabriel Lebec <glebec@gmail.com>
2018-08-31 11:08:19 -0400

    day 3: more performant (but uglier) PUT route

--- server/api/index.js
+++ server/api/index.js
@@ -14,10 +14,18 @@ router.get('/coins', async (req, res, next) => {
 
 router.put('/coins/:id', async (req, res, next) => { // BODY: { name: String }
   try {
-    const coin = await Coin.findById(req.params.id)
-    const updatedCoin = await coin.update({
+    const [, [updatedCoin]] = await Coin.update({
       name: req.body.name
+    }, {
+      where: {
+        id: req.params.id
+      },
+      returning: true
     })
+    // const coin = await Coin.findById(req.params.id)
+    // const updatedCoin = await coin.update({
+    //   name: req.body.name
+    // })
     res.json(updatedCoin)
   } catch (err) {
     next(err)
